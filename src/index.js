f558fe1d51b9a859ecaeea93d901b85117396255
Dmitri611 <dimakkk1181@mail.ru>
2021-02-04 12:44:18 +0300

    feat: finish task

---
+++ src/index.js
@@ -0,0 +1,23 @@
+
+exports.min = function min (array) {
+    if (!Array.isArray(array) || array.length <=0) return 0;
+    return Math.min(...array);
+}
+
+exports.max = function max (array) {
+    if (!Array.isArray(array) || array.length <=0) return 0;
+    return Math.max(...array);
+}
+
+Math.avg = (arr) => {
+  let sum = 0;
+  arr.forEach(element => {
+      sum += element;
+  });
+  return sum/arr.length;
+}
+
+exports.avg = function avg (array) {
+    if (!Array.isArray(array) || array.length <=0) return 0;
+    return Math.avg(array);
+}
